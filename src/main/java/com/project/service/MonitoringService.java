19f4d521f32b2bf5cd9f6ea34f36101b075e6cdc
broxzx <illyasedelnikov123@gmail.com>
2024-09-13 16:15:36 +0200

    refactored

--- src/main/java/com/project/service/MonitoringService.java
+++ src/main/java/com/project/service/MonitoringService.java
@@ -4,32 +4,35 @@ import com.profesorfalken.jsensors.model.components.Components;
 import com.profesorfalken.jsensors.model.sensors.Sensors;
 import com.profesorfalken.jsensors.model.sensors.Temperature;
 import com.project.utils.ComponentConfiguration;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.List;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
+@Slf4j
 public class MonitoringService {
 
     private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
 
     public void getComponentsData() {
-        Thread thread = new Thread(() -> {
-            System.out.printf("Current Thread: %s%n", Thread.currentThread().getName());
+        scheduler.scheduleAtFixedRate(() -> {
+            log.info("Current Thread: %s".formatted(Thread.currentThread().getName()));
             Components components = ComponentConfiguration.getComponents();
+
+            if (components == null) {
+                throw new RuntimeException("Couldn't find components");
+            }
 
             processCpu(components);
             processGpu(components);
-        });
-
-
-        scheduler.scheduleAtFixedRate(thread, 0, 30, TimeUnit.SECONDS);
+        }, 0, 5, TimeUnit.MINUTES);
     }
 
     private void processCpu(Components components) {
         components.cpus.forEach(currentCpu -> {
-            System.out.printf("Getting sensors for cpu %s%n", currentCpu.name);
+            log.info("Getting sensors for cpu %s".formatted(currentCpu.name));
 
             displayData(currentCpu.sensors);
         });
@@ -37,7 +40,7 @@ public class MonitoringService {
 
     private void processGpu(Components components) {
         components.gpus.forEach(currentGpu -> {
-            System.out.printf("Getting sensors for gpu %s%n", currentGpu.name);
+            log.info("Getting sensors for gpu %s".formatted(currentGpu.name));
 
             displayData(currentGpu.sensors);
         });
@@ -45,13 +48,40 @@ public class MonitoringService {
 
     private void displayData(Sensors sensors) {
         displayTemperature(sensors.temperatures);
-        System.out.println("-".repeat(50));
+        log.info("-".repeat(50));
     }
 
     private void displayTemperature(List<Temperature> temperatures) {
         temperatures
                 .forEach(temperature -> {
-                    System.out.println(temperature.name + " temp: " + temperature.value);
+                    Double tempValue = temperature.value;
+                    String tempName = temperature.name;
+
+                    System.out.println(tempName + " temp: " + tempValue);
+                    checkTemperature(tempValue);
                 });
+    }
+
+    private void checkTemperature(double temperature) {
+        if (temperature >= 70) {
+            String shutdownCommand;
+            String operatingSystem = System.getProperty("os.name");
+
+            if ("Linux".equals(operatingSystem) || "Mac OS X".equals(operatingSystem)) {
+                shutdownCommand = "shutdown -h now";
+            } else if (operatingSystem.contains("Windows")) {
+                shutdownCommand = "shutdown.exe -s -t 0";
+            } else {
+                throw new RuntimeException("Unsupported operating system.");
+            }
+
+            try {
+                Runtime.getRuntime().exec(shutdownCommand);
+            } catch (Exception exception) {
+                log.error(exception.getMessage());
+            }
+
+            System.exit(0);
+        }
     }
 }
